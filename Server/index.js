0b0b37e9100584cdd3fe52ef77a1ca419467dadc
madhukumarap <madhukumarap07@gmail.com>
2024-01-26 18:11:13 +0530

    Dd

--- Server/index.js
+++ Server/index.js
@@ -32,15 +32,35 @@ app.get("/", async (req, res) => {
 // create data and save in data in mongoose
 app.post("/create", (req, res) => {
     const newUser = new userModel(req.body);
-    newUser.save()
+    const data = newUser.save()
         .then(() => {
-            res.json({ success: true, data: "data sent successfully" });
+            res.json({ success: true, data: data});
         })
         .catch((error) => {
             res.status(500).json({ success: false, error: error.message });
         });
 });
-
+//update
+app.put("/update",async(req, res)=>{
+    console.log(req.body)
+    const {id,...rest} = req.body
+    const data = await userModel.updateOne({
+        _id:id
+    },rest)
+    res.send({
+        success:true, data:data
+    })
+})
+//deleete
+app.delete("/delete/:id", async (req, res)=>{
+    const id = req.params.id;
+    console.log(id )
+    const data = await userModel.deleteOne({_id :id})
+    res.send({
+        success:true, data:data,
+        message:"delete succesfully"
+    })
+})
 mongoose.connect('mongodb://127.0.0.1:27017/crudoperation')
     .then(() => {
         console.log("connected to db");
